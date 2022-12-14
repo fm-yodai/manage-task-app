4898d5be9de2607f01551e786571437f473f3deb
fm-yodai <yodai@freemind.co.jp>
2022-12-14 18:10:36 +0900

    リスト新規作成 削除まで

---
+++ src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list">
+    <div class="listheader">
+      <p class="list-title">{{ title }}</p>
+      <div class="deletelist" @click="removeList">Delete</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: true
+    },
+    listIndex: {
+      required: true
+    }
+  },
+  methods: {
+    removeList () {
+      if (confirm('majide kesu??')) {
+        this.$store.dispatch('removeList', { title: this.title })
+      }
+    }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -10,13 +10,26 @@ const vuexPersist = new VuexPersistence({
 
 export default new Vuex.Store({
   state: {
+    taskLists: []
   },
   getters: {
   },
   mutations: {
     RESTORE_MUTATION: vuexPersist.RESTORE_MUTATION,
     addList (state, payload) {
+      state.taskLists.push({ title: payload.title, cards:[] })
+    },
+    removeList (state, payload) {
+      state.taskLists = state.taskLists.filter(list => list.title !== payload.title)
+    }
   },
   actions: {
+    addList (context, payload) {
+      context.commit('addList', payload)
+    },
+    removeList (context, payload) {
+      context.commit('removeList', payload)
+    }
+  },
   plugins: [vuexPersist.plugin]
 })

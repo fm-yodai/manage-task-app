64fd1e720cd655414907a8486bb7f0fc7abf7470
fm-yodai <yodai@freemind.co.jp>
2022-12-14 14:21:15 +0900

    ボード作成

---
+++ src/views/BoardView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <BoardComponent />
+  </div>
+</template>
+
+<script>
+import BoardComponent from '@/components/Board.vue'
+
+export default {
+  name: 'BoardView',
+  components: {
+    BoardComponent
+  }
+}
+</script>
